288c35259e8e4ba2739f53f427600cc51b2b3799
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-16 20:27:14 -0500

    finally finished

---
+++ Desktop/react-crash-course/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/title.jsx'
+function App() {
+  return (
+    <div>
+<Title></Title>    <Todo />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/react-crash-course/src/components/Todo.jsx
@@ -0,0 +1,10 @@
+function Todo(){
+    return (
+    <div>
+      <h2> Finish FrontendSimplified</h2>
+      <button>Delete</button>
+     </div>
+    )
+}
+
+export default Todo
